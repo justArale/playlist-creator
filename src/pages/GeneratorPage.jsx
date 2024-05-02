7c2206dddb2f53f86a794a13d3d72fde64fc8e16
camilaalbuq <cam.albuquer@gmail.com>
2024-05-02 17:53:36 +0200

    initial related artists functionality

--- src/pages/GeneratorPage.jsx
+++ src/pages/GeneratorPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import GeneratorBox from "../components/GeneratorBox";
 import Button from "../components/Button";
+import RelatedArtists from "../components/RelatedArtists";
 
 function GeneratorPage() {
   return (
     <>
       <GeneratorBox />
+      <RelatedArtists />
     </>
   );
 }
