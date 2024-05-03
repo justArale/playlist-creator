1eaf3067236cf6f2ec2ba50a279a71ef983b7561
camilaalbuq <cam.albuquer@gmail.com>
2024-05-03 13:21:35 +0200

    generator page conflicts

--- src/pages/GeneratorPage.jsx
+++ src/pages/GeneratorPage.jsx
@@ -2,13 +2,10 @@ import React, { useState } from "react";
 import GeneratorBox from "../components/GeneratorBox";
 import Button from "../components/Button";
 
-
 function GeneratorPage() {
-
   return (
     <>
       <GeneratorBox />
-      <RangeButton />
     </>
   );
 }
