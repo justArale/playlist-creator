becf1129523aa6d739b90ec8878c81def2d835b3
camilaalbuq <cam.albuquer@gmail.com>
2024-05-04 17:55:42 +0200

    footer text size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,13 @@
 
 function Footer() {
   return (
-    <footer className="fixed bottom-0 w-full bg-white text-gray-900 text-center justify-center p-2">
+    <footer className="fixed bottom-0 w-full justify-center bg-white p-3 text-center text-sm text-gray-900">
       {/* <div className="w-full bg-white text-gray-900 text-center justify-center"> */}
       <a href="https://github.com/justArale/playlist-creator">
-        playlist-creator
+        playlist-creator{" | "}
       </a>{" "}
-      made with ❤️ and 🎵 by <a href="https://github.com/annagy07">annagy07</a>
+      made with ❤️ {"   "} and{"  "} 🎵 by{"  "}
+      <a href="https://github.com/annagy07">annagy07</a>
       {" | "}
       <a href="https://github.com/camialbuq">Cami Albuquerque</a>
       {" | "}
