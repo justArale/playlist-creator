b7c7448766b64cbbde45aedda98d565c3a902fb7
camilaalbuq <cam.albuquer@gmail.com>
2024-05-06 16:19:20 +0200

    search page design done

--- src/components/SearchArtist.jsx
+++ src/components/SearchArtist.jsx
@@ -81,17 +81,21 @@ const SearchArtist = ({ getArtistId, getAccessToken, accessToken }) => {
   //   getArtistId(selectedArtist.id)
 
   return (
-    <div classname="bg-red-900">
+    <div>
       {!selectedArtist && (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="">
           <input
             type="text"
             value={artistNameInput}
             onChange={(e) => setArtistNameInput(e.target.value)}
             placeholder="Enter artist name"
             list="artists" // Associate input with datalist
+            className="w-full rounded-lg border border-violet-500 bg-gray-100 px-4 py-2.5 text-sm text-gray-900 focus:border-violet-500 focus:ring-2 focus:ring-violet-500 lg:max-w-2xl lg:text-xl"
           />
-          <datalist id="artists">
+          <datalist
+            id="artists"
+            className="w-full rounded-lg border border-violet-300 bg-gray-100 px-4 py-2.5 text-sm text-gray-900 focus:border-violet-500 focus:ring-2 focus:ring-violet-500"
+          >
             {" "}
             {/* Datalist for autocomplete options */}
             {searchResults.map((artist) => (
@@ -99,20 +103,30 @@ const SearchArtist = ({ getArtistId, getAccessToken, accessToken }) => {
                 key={artist.id}
                 value={artist.name}
                 onMouseDown={() => handleOptionClick(artist)} // Use onMouseDown instead of onClick
+                className="w-full rounded-lg border border-violet-300 bg-gray-100 px-4 py-2.5 text-sm text-gray-900 focus:border-violet-500 focus:ring-2 focus:ring-violet-500"
               />
             ))}
           </datalist>
           <br></br>
-          <button type="submit" className="find-artist-button">
+          <button
+            type="submit"
+            className="my-6 rounded-xl bg-violet-300 px-5 py-2 text-sm uppercase text-violet-900 active:underline xl:text-lg"
+          >
             Choose this Artist
           </button>
         </form>
       )}
       {selectedArtist && (
-        <div>
-          <p>Selected Artist:</p>
-          <p>{selectedArtist.name}</p>
-          <img src={selectedArtist.images[0]?.url} alt={selectedArtist.name} />
+        <div className="col-1 grid items-center justify-center">
+          <p className="md:text-lg lg:text-xl">Selected Artist:</p>
+          <p className="my-1 text-lg font-semibold text-violet-900 md:my-3 md:text-3xl lg:text-4xl">
+            {selectedArtist.name}
+          </p>
+          <img
+            src={selectedArtist.images[0]?.url}
+            alt={selectedArtist.name}
+            className="my-3 h-full w-full rounded-xl object-cover md:mb-6 md:max-h-80 md:max-w-lg lg:max-h-96 lg:max-w-xl"
+          />
         </div>
       )}
     </div>
