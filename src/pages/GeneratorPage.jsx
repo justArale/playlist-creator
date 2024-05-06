516b0957cbd44b534f5dcba223d234988db05cef
justArale <just1arale@gmail.com>
2024-05-06 15:23:54 +0200

    fetch user data and user playlist

--- src/pages/GeneratorPage.jsx
+++ src/pages/GeneratorPage.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import GeneratorBox from "../components/GeneratorBox";
+import SavePlaylistOnSpotify from "../components/SavePlaylistOnSpotify";
 
 function GeneratorPage() {
   return (
     <div className="my-60">
       <GeneratorBox />
+      <SavePlaylistOnSpotify />
     </div>
   );
 }
