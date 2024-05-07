d3a07efdc75b6535c122e0224f92773bbf0665f9
justArale <just1arale@gmail.com>
2024-05-07 18:57:56 +0200

    appearing overlay with connecting to spotifylogin

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,21 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import "./Overlay.css";
-import SpotifyLogin from './SpotifyLogin'; // Importiere die SpotifyLogin-Komponente hier
 import overlayImg from '../assets/Graphic.png';
+
+////777//
+
+const CLIENT_ID = `${import.meta.env.VITE_CLIENT_ID}`;
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize?";
+const REDIRECT_URL_AFTER_LOGIN = "http://localhost:5173/results";
+const SPACE = "%20";
+const SCOPES = ["user-read-private", "user-read-email", "playlist-read-private", "playlist-modify-public"];
+const SCOPES_URL_PARAMS = SCOPES.join(SPACE);
+
+const getReturnedParamsFromSpotifyAuth = (hash) => {
+  const stringAfterHashtag = hash.substring(1);
+  const paramsInUrl = stringAfterHashtag.split("&");
+  const paramsSplitUp = paramsInUrl.reduce((acc, currValue) => {
+    const [key, value] = currValue.split("=");
+    acc[key] = value;
+    return acc;
+  }, {});
+  return paramsSplitUp;
+};
 
 function Overlay({isOpen, onClose, createPlaylist}) {
     const [token, setToken] = useState(localStorage.getItem("accessToken"));
+
+    useEffect(() => {
+        if (window.location.hash) {
+          const { access_token, expires_in, token_type } =
+            getReturnedParamsFromSpotifyAuth(window.location.hash);
+          localStorage.clear();
+          localStorage.setItem("accessToken", access_token);
+          localStorage.setItem("expiresIn", expires_in);
+          localStorage.setItem("tokenType", token_type);
+        }
+      });
+      const handleLogin = () => {
+        window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAMS}&response_type=token&show_dialoge=true`;
+      };
 
     const handleConnectAndSave = () => {
         if (token) {
           createPlaylist();
         } else {
-          SpotifyLogin();
+            handleLogin();
         }
       };
 
       useEffect(() => {
-        // Füge einen Event-Listener hinzu, um das Overlay zu schließen, wenn außerhalb geklickt wird
+        // Event-Listener to close the overlay if you click outside
         const handleClickOutside = (event) => {
             if (isOpen && !event.target.closest(".overlay-container")) {
                 onClose();
