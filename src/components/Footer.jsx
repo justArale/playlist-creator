799231ca597369c33a4ef425f5988e033e8c6205
justArale <just1arale@gmail.com>
2024-05-07 20:11:41 +0200

    finished footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
-// import "./Footer.css";
+import "./Footer.css";
 
 function Footer() {
   return (
     <footer className="inset-x-0 bottom-0 mb-2 w-full justify-center bg-white px-3 py-2 text-center text-sm text-gray-900 md:mb-10 md:pb-6 md:text-lg lg:text-2xl ">
       {/* <div className="w-full bg-white text-gray-900 text-center justify-center"> */}
-      <a href="https://github.com/justArale/playlist-creator">
-        playlist-creator{" | "}
+      <p className="footer-credits"><a href="https://github.com/justArale/playlist-creator">
+        Spotify Project
       </a>{" "}
-      made with ❤️ {"   "} and{"  "} 🎵 by{"  "}
-      <a href="https://github.com/annagy07">annagy07</a>
-      {" | "}
-      <a href="https://github.com/camialbuq">Cami Albuquerque</a>
+      made with 💜 {"   "}by{"  "}
+      <a href="https://github.com/annagy07">Anna</a>
       {" | "}
       <a href="https://github.com/justArale">Arale</a>
+      {" | "}
+      <a href="https://github.com/camialbuq">Cami</a></p>
       {/* </div> */}
     </footer>
   );
