7b920f111dae6a93fe8b94dbce1250c78dbf9f21
justArale <just1arale@gmail.com>
2024-05-07 17:21:39 +0200

    Complete overlay attempt commented out

--- src/components/SavePlaylistOnSpotify.jsx
+++ src/components/SavePlaylistOnSpotify.jsx
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Overlay from './Overlay';
+// import Overlay from './Overlay';
 
-function SavePlaylistOnSpotify({ results }) {
-  const [token, setToken] = useState(localStorage.getItem("accessToken"));
-  const [userId, setUserId] = useState("");
-  const [playlistId, setPlaylistId] = useState("");
-  const [isOverlayOben, setIsOverlayOben] = useState(false);
+
+function SavePlaylistOnSpotify({results}) {
+    const [token, setToken] = useState(localStorage.getItem("accessToken"));
+    const [userId, setUserId] = useState("");
+    const [playlistId, setPlaylistId] = useState("");
+    // const [isOverlayOben, setIsOverlayOben] = useState(false);
 
   const PLAYLIST_ENDPOINT = `https://api.spotify.com/v1/users/${userId}/playlists`;
   const TRACK_ENDPOINT = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
@@ -38,97 +39,99 @@ function SavePlaylistOnSpotify({ results }) {
       });
   };
 
-  useEffect(() => {
-    if (token) {
-      getUserInfo();
-    }
-  }, [token]);
-
-  // const createPlaylist = async () => {
-  //   try {
-  //     const response = await axios.post(
-  //       PLAYLIST_ENDPOINT,
-  //       {
-  //         name: `${getArtistInput} ${getMoodInput} Playlist`,
-  //         description: "Created from my app",
-  //         public: true,
-  //       },
-  //       {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //           "Content-Type": "application/json",
-  //         },
-  //       },
-  //     );
-  //     setPlaylistId(response.data.id); // Save the new playlist ID for further use
-  //     console.log("Playlist created successfully!");
-  //   } catch (error) {
-  //     console.log("Error creating playlist:", error);
-  //   }
-  // };
-
+    useEffect(() => {
+        if (token) {
+            getUserInfo();
+        }
+    }, [token]);
 
     const createPlaylist = async () => {
-      if (!token) {
-        setIsOverlayOben(true);
-    } else {
       try {
-      const response = await axios.post(
-        PLAYLIST_ENDPOINT,
-        {
-          name: `${getArtistInput} ${getMoodInput} Playlist`,
-          description: "Created from my app",
-          public: true
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json'
+        const response = await axios.post(
+          PLAYLIST_ENDPOINT,
+          {
+            name: `${getArtistInput} ${getMoodInput} Playlist`,
+            description: "Created with Playlist-Creator from Anna, Cami and Arale",
+            public: true
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'application/json'
+            }
           }
-        }
-      );
-      setPlaylistId(response.data.id); // Save the new playlist ID for further use
-      console.log('Playlist created successfully!');
-    } catch (error) {
-    setShowOverlay(true);
-      console.log('Error creating playlist:', error);
-    }}
-        
-      };
+        );
+        setPlaylistId(response.data.id); // Save the new playlist ID for further use
+        console.log('Playlist created successfully!');
+      } catch (error) {
+        console.log('Error creating playlist:', error);
+      }
+    };
 
-      useEffect(() => {
-        const setTracksToPlaylist = async () => {
-          if (!playlistId) return
-  
-          const trackURIs = results.map(track => track.uri)
-          console.log(results)
-          try {
-            await axios.post(
-              TRACK_ENDPOINT,
-              {
-                  uris: trackURIs, // Pass the array directly
-                  position: 0
-                },
-              {
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                  'Content-Type': 'application/json'
-                }
+
+    // const createPlaylist = async () => {
+    //   if (!token) {
+    //     setIsOverlayOben(true);
+    // } else {
+    //   try {
+    //   const response = await axios.post(
+    //     PLAYLIST_ENDPOINT,
+    //     {
+    //       name: `${getArtistInput} ${getMoodInput} Playlist`,
+    //       description: "Created from my app",
+    //       public: true
+    //     },
+    //     {
+    //       headers: {
+    //         Authorization: `Bearer ${token}`,
+    //         'Content-Type': 'application/json'
+    //       }
+    //     }
+    //   );
+    //   setPlaylistId(response.data.id); // Save the new playlist ID for further use
+    //   console.log('Playlist created successfully!');
+    // } catch (error) {
+    // setShowOverlay(true);
+    //   console.log('Error creating playlist:', error);
+    // }}
+        
+    //   };
+
+    useEffect(() => {
+      const notify = () => toast("Playlist is now in your Spotify library!");
+      const setTracksToPlaylist = async () => {
+        if (!playlistId) return
+
+        const trackURIs = results.map(track => track.uri)
+        console.log(results)
+        try {
+          await axios.post(
+            TRACK_ENDPOINT,
+            {
+                uris: trackURIs, // Pass the array directly
+                position: 0
+              },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json'
               }
-            );
-            console.log('Tracks added successfully!');
-          } catch (error) {
-            console.log('Error adding tracks to playlist:', error);
-          }
+            }
+          );
+          console.log('Tracks added successfully!');
+        } catch (error) {
+          console.log('Error adding tracks to playlist:', error);
         }
-        setTracksToPlaylist();
-      }, [playlistId])
-  
+      }
+      setTracksToPlaylist();
+      notify();
+    }, [playlistId])
+
 
     return (
         <div>
              <button className="px-3 py-1" onClick={() => {createPlaylist()}}>+ Save to my Spotify</button>
-             <Overlay isOpen={isOverlayOben} onClose={()=> setIsOverlayOben(!isOverlayOben)} createPlaylist={createPlaylist}/>
+             {/* <Overlay isOpen={isOverlayOben} onClose={()=> setIsOverlayOben(!isOverlayOben)} createPlaylist={createPlaylist}/> */}
             <ToastContainer />
         </div>
     );
